59b8b2847e2f34313c48f9cdd7bd047925170586
crackcomm <crackcomm@gmail.com>
2014-08-15 02:13:21 +0200

    feature: stream support

--- testing/testing.go
+++ testing/testing.go
@@ -3,6 +3,8 @@ package testing
 import (
 	"github.com/crackcomm/go-actions/action"
 	"github.com/crackcomm/go-actions/local"
+	"io/ioutil"
+	"io"
 )
 
 // DefaultRunner - Test action default runner.
@@ -31,6 +33,16 @@ func (t *Test) Run() *Result {
 
 	// Run action using default runner
 	ctx, err := DefaultRunner.Run(a)
+
+	for key, value := range ctx {
+		if rc, ok := value.(io.ReadCloser); ok {
+			arr, _ := ioutil.ReadAll(rc)
+			rc.Close()
+			ctx[key] = arr
+		}
+	}
+
+	ctx.Close()
 
 	// Result collection end
 	result.End(ctx, err)
